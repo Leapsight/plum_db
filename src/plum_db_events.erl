43a4ed35d1002904bb7436ab1928b6023fb5c87e
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-08-20 08:16:10 +0100

    fix copyright notice

--- src/plum_db_events.erl
+++ src/plum_db_events.erl
@@ -1,22 +1,20 @@
-%% -------------------------------------------------------------------
+%% =============================================================================
+%%  plum_db_events.erl -
 %%
-%% Copyright (c) 2017 Ngineo Limited t/a Leapsight.  All Rights Reserved.
+%%  Copyright (c) 2017-2019 Ngineo Limited t/a Leapsight. All rights reserved.
 %%
-%% This file is provided to you under the Apache License,
-%% Version 2.0 (the "License"); you may not use this file
-%% except in compliance with the License.  You may obtain
-%% a copy of the License at
+%%  Licensed under the Apache License, Version 2.0 (the "License");
+%%  you may not use this file except in compliance with the License.
+%%  You may obtain a copy of the License at
 %%
-%%   http://www.apache.org/licenses/LICENSE-2.0
+%%     http://www.apache.org/licenses/LICENSE-2.0
 %%
-%% Unless required by applicable law or agreed to in writing,
-%% software distributed under the License is distributed on an
-%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-%% KIND, either express or implied.  See the License for the
-%% specific language governing permissions and limitations
-%% under the License.
-%%
-%% -------------------------------------------------------------------
+%%  Unless required by applicable law or agreed to in writing, software
+%%  distributed under the License is distributed on an "AS IS" BASIS,
+%%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%  See the License for the specific language governing permissions and
+%%  limitations under the License.
+%% =============================================================================
 
 -module(plum_db_events).
 
@@ -169,10 +167,12 @@ notify(Event, Message) ->
     gen_event:notify(?MODULE, {Event, Message}).
 
 
+
 %% =============================================================================
 %% GEN_EVENT CALLBACKS
 %% This is to support adding a fun via add_callback/1 and add_sup_callback/1
 %% =============================================================================
+
 
 
 init([pubsub]) ->
