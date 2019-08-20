4b899590ab28c3865729468bd41e5629e0479b91
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2019-08-20 08:09:47 +0100

    added draft coordinator

---
+++ src/plum_db_startup_coordinator.erl
@@ -0,0 +1,120 @@
+%% =============================================================================
+%%  plum_db_startup_coordinator.erl -
+%%
+%%  Copyright (c) 2016-2019 Ngineo Limited t/a Leapsight. All rights reserved.
+%%
+%%  Licensed under the Apache License, Version 2.0 (the "License");
+%%  you may not use this file except in compliance with the License.
+%%  You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%%  Unless required by applicable law or agreed to in writing, software
+%%  distributed under the License is distributed on an "AS IS" BASIS,
+%%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%  See the License for the specific language governing permissions and
+%%  limitations under the License.
+%% =============================================================================
+
+-module(plum_db_startup_coordinator).
+-behaviour(gen_server).
+
+-record(state, {
+    partitions   ::  list()
+}).
+
+-export([start_link/0]).
+%% -export([])
+
+%% gen_server callbacks
+-export([init/1]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([terminate/2]).
+-export([code_change/3]).
+
+
+
+%% =============================================================================
+%% API
+%% =============================================================================
+
+
+
+%% -----------------------------------------------------------------------------
+%% @doc Start plumtree_partitions_coordinator and link to calling process.
+%% @end
+%% -----------------------------------------------------------------------------
+-spec start_link() -> {ok, pid()} | ignore | {error, term()}.
+
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+
+
+%% =============================================================================
+%% GEN_SERVER_ CALLBACKS
+%% =============================================================================
+
+
+
+%% @private
+-spec init([]) ->
+    {ok, state()}
+    | {ok, state(), non_neg_integer() | infinity}
+    | ignore
+    | {stop, term()}.
+
+init([]) ->
+    State = #state{partitions = plum_db:partitions()},
+    %% plum_db_events:
+    {ok, State}.
+
+
+%% @private
+-spec handle_call(term(), {pid(), term()}, state()) ->
+    {reply, term(), state()}
+    | {reply, term(), state(), non_neg_integer()}
+    | {noreply, state()}
+    | {noreply, state(), non_neg_integer()}
+    | {stop, term(), term(), state()}
+    | {stop, term(), state()}.
+
+handle_call(_Event, _From, State) ->
+    Res = ok,
+    {reply, Res, State}.
+
+
+%% @private
+-spec handle_cast(term(), state()) ->
+    {noreply, state()}
+    | {noreply, state(), non_neg_integer()}
+    | {stop, term(), state()}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+
+%% @private
+-spec handle_info(term(), state()) ->
+    {noreply, state()}
+    | {noreply, state(), non_neg_integer()}
+    | {stop, term(), state()}.
+
+handle_info(_Event, State) ->
+    {noreply, State}.
+
+
+%% @private
+-spec terminate(term(), state()) -> term().
+
+terminate(_Reason, _State) ->
+    ok.
+
+
+%% @private
+-spec code_change(term() | {down, term()}, state(), term()) -> {ok, state()}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
