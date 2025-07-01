683b5062df76a08cbd37ab046866c65164f6ccfe
Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>
2025-07-02 00:08:08 +0100

    crash hashtree is server crashes
    Signed-off-by: Alejandro M. Ramallo <alejandro.ramallo@leapsight.com>

--- src/plum_db_partition_sup.erl
+++ src/plum_db_partition_sup.erl
@@ -56,7 +56,7 @@ name(Id) when is_integer(Id) ->
 
 
 init([Id, ServerOpts, HashtreeOpts]) ->
-    RestartStrategy = {one_for_all, 20, 60},
+    RestartStrategy = {rest_for_one, 20, 60},
     Children = [
         #{
             id => plum_db_partition_server:name(Id),
